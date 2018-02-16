bf4b6d4c31dd45f7e8ac127c3a5236d1a3820931
Johann Blake <johannblake@gmail.com>
2018-02-16 12:23:22 +0100

    Added CopyBuild task to combine builds

--- company-service/runApp.sh
+++ company-service/runApp.sh
@@ -1,2 +1,2 @@
-dev_appserver.sh --address=0.0.0.0 --port=8080 --disable_update_check --jvm_flag=-Xdebug --jvm_flag=-Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8000 build/exploded-Accounts
+dev_appserver.sh --address=0.0.0.0 --port=8080 --disable_update_check --jvm_flag=-Xdebug --jvm_flag=-Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8000 build/exploded-company-service
 
